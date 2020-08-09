47f25d9bf42a8be22124308e286f24c7d5687bfa
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-08-08 21:11:55 -0500

    Project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider>
+            <CSSReset />
+            <ColorModeProvider>
+                <App />
+            </ColorModeProvider>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
