e2d69c82cd0168ee7dd5ee6ea0fe08e31ceae41e
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-08-11 16:10:01 -0500

    Added pagination

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item } from '../Item';
 import { Search } from '../Search';
-import { Grid, Flex, Heading, Box } from '@chakra-ui/core';
+import {
+    Grid,
+    Flex,
+    Heading,
+    Box,
+    Stack,
+    Button,
+    Tag,
+    Select,
+    useColorMode,
+} from '@chakra-ui/core';
 import { Item as IItem } from '../../types/spoilerLog';
 import sortBy from 'lodash/sortBy';
 
@@ -17,8 +27,35 @@ const ItemList = ({
     itemList,
     hideSearch = false,
 }: ItemListProps) => {
-    const sortedData = sortBy(itemList, 'location'),
-        [filteredData, setFilteredData] = useState<IItem[]>(sortedData);
+    const { colorMode } = useColorMode(),
+        bgColor = {
+            dark: 'gray.800',
+            light: 'gray.200',
+        },
+        borderColor = { dark: 'gray.600', light: 'gray.300' },
+        sortedData = sortBy(itemList, 'location'),
+        [filteredData, setFilteredData] = useState<IItem[]>(sortedData),
+        [windowedData, setWindowedData] = useState<IItem[]>([]),
+        [numPages, setNumPages] = useState(0),
+        [currentPage, setCurrentPage] = useState(0),
+        pageSizeOptions = [25, 50, 75, 100],
+        [pageSize, setPageSize] = useState(50);
+
+    const handlePageSizeChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setPageSize(parseInt(event.target.value));
+    };
+
+    useEffect(() => {
+        setNumPages(Math.ceil(filteredData.length / pageSize));
+        setWindowedData(
+            filteredData.slice(
+                currentPage * pageSize,
+                currentPage * pageSize + pageSize
+            )
+        );
+    }, [filteredData, pageSize, currentPage]);
 
     return (
         <Flex direction="column" width="100%">
@@ -43,13 +80,80 @@ const ItemList = ({
                 templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
                 gap={2}
             >
-                {filteredData.map((item) => (
+                {windowedData.map((item) => (
                     <MemoSpoiler
                         key={`${item.location}${item.item}`}
                         {...item}
                     />
                 ))}
             </Grid>
+            {numPages > 1 && (
+                <Stack
+                    isInline
+                    width="95%"
+                    position="sticky"
+                    bottom={2}
+                    marginTop="0.5em"
+                    justify="space-between"
+                    align="center"
+                    bg={bgColor[colorMode]}
+                    borderColor={borderColor[colorMode]}
+                    borderWidth={2}
+                    padding="0.25em"
+                    borderRadius={5}
+                    alignSelf="center"
+                    boxShadow="0 3px 5px rgba(0,0,0,0.15)"
+                >
+                    <Box>
+                        <Select
+                            size="sm"
+                            value={pageSize}
+                            onChange={handlePageSizeChange}
+                        >
+                            {pageSizeOptions.map((option) => (
+                                <option key={option} value={option}>
+                                    {option} items per page
+                                </option>
+                            ))}
+                        </Select>
+                    </Box>
+                    <Tag size="sm">
+                        Page {currentPage + 1} of {numPages}
+                    </Tag>
+                    <Stack isInline>
+                        <Button
+                            size="sm"
+                            isDisabled={currentPage === 0}
+                            onClick={() => setCurrentPage(0)}
+                        >
+                            <i className="fas fa-angle-double-left" />
+                        </Button>
+                        <Button
+                            size="sm"
+                            isDisabled={currentPage === 0}
+                            onClick={() => setCurrentPage(currentPage - 1)}
+                        >
+                            <i className="fas fa-angle-left" />
+                            &nbsp;Back
+                        </Button>
+                        <Button
+                            size="sm"
+                            isDisabled={currentPage + 1 === numPages}
+                            onClick={() => setCurrentPage(currentPage + 1)}
+                        >
+                            Next&nbsp;
+                            <i className="fas fa-angle-right" />
+                        </Button>
+                        <Button
+                            size="sm"
+                            isDisabled={currentPage + 1 === numPages}
+                            onClick={() => setCurrentPage(numPages - 1)}
+                        >
+                            <i className="fas fa-angle-double-right" />
+                        </Button>
+                    </Stack>
+                </Stack>
+            )}
         </Flex>
     );
 };
